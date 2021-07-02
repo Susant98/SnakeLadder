a477e959e7398ad66668b7972f7a5fd52780471d
Rajanahak1998 <rajanahak1998@gmail.com>
2021-07-02 20:24:27 +0530

    Added- Iterate to reach winning position

--- SnakeUC4.java
+++ SnakeUC4.java
@@ -1,32 +1,33 @@
-import java.util.*;
-public class SnakeUC3 {
+import java.util.Scanner;
+public class SnakeUC4 {
+	public static final int winningPosition = 100;
 	public static void main(String[] args) {
 		System.out.print("Enter player name: ");
 		Scanner s = new Scanner(System.in);
 		String playerName = s.next();
 		int playerPosition = 0;
 		System.out.println("Player name: " + playerName);
-		//using random to get number between 1 to 6.
+		
+		while ( winningPosition > playerPosition ) {
 		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Dice roll: " + die);
-		//using random to check for option.
+		
 		int option = (int) (Math.floor(Math.random() * 10) % 3);
 		switch (option) {
 			case 0:
-				System.out.println("No play"); //if no play player stay on same position
+				System.out.println("No play : " + 0);
 				playerPosition += 0;
 				break;
 			case 1:
-				System.out.println("Ladder"); //if it is ladder then it will move that position in he die.
+				System.out.println("Ladder  : " + "+" + die);
 				playerPosition += die;
 				break;
 			default:
-				System.out.println("Snake");//if it is snake it will move behind the number of position
+				System.out.println("Snake   : " + "-" + die);
 				playerPosition -= die;
 				if (playerPosition < 0)
-					playerPosition = 0;		
+					playerPosition = 0;
+		}
 		}
 		System.out.println("Player position: " + playerPosition);
 	}
-
 }
